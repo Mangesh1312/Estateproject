d445415562cf53f47050a661480def3589c1c60c
Mangesh1312 <mangeshnakhate13@gmail.com>
2026-08-12 12:59:13 +0530

    If the Changes section is empty, Visual Studio doesn't currently detect any Git changes

---
+++ src/AuctionService/Data/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Entities.Auction> Auctions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Property>().ToTable("Properties");
+            modelBuilder.Entity<Entities.Auction>()
+                .HasOne(x => x.Property)
+                .WithOne(x => x.Auction)
+                .HasForeignKey<Entities.Property>(p => p.Auctionid);
+        }
+    }
+}
---
+++ src/AuctionService/Entities/Property.cs
@@ -0,0 +1,24 @@
+﻿namespace AuctionService.Entities
+{
+    public class Property
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+
+        public string State { get; set; }
+
+        public string City { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public decimal StartingPrice { get; set; }
+
+        public decimal AreaSqFt { get; set; }
+         
+        public Guid Auctionid { get; set; }
+
+        public Auction? Auction { get; set; }
+    }
+}
